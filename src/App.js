2b24e30118471e9bf5590dfc64222e874b02345c
Олександр Вдовенко <Alex.Breid2004@gmail.com>
2025-10-27 21:49:23 +0200

    Initial commit of the project structure

--- src/App.js
+++ src/App.js
@@ -2,14 +2,14 @@ import React from 'react';
 import { Routes, Route } from 'react-router-dom'; // ← Добавь импорт
 import Work from './pages/work'; // ← Импортируй компонент Work
 import Epidemy from './pages/epidemy'; 
-import Quiz_tashkent from './pages/quiz_tashkent'; 
+import QuizTashkent from './pages/QuizTashkent'; 
 function App() {
   return (
     <div className="App">
       <Routes>
         <Route path="/work" element={<Work />} />
         <Route path="/epidemy" element={<Epidemy />} />
-        <Route path="/Quiz_tashkent" element={<Quiz_tashkent />} />
+        <Route path="/QuizTashkent" element={<QuizTashkent />} />
         {/* Добавь другие маршруты сюда */}
         {/* <Route path="/" element={<Home />} /> если у тебя есть главная страница */}
       </Routes>
